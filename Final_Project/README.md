3a8747089c7893430e9b87564aa03a16fbadbfda
Faith Le <lefaith12661@gmail.com>
2023-04-19 17:59:26 -0400

    Final Project from CS50

---
+++ Final_Project/README.md
@@ -0,0 +1,163 @@
+# $ REVIEW
+#### Video Demo:  <https://www.youtube.com/watch?v=_W64huDytzk>
+<br />
+
+### DESCRIPTION
+------------
+$ Review is a web-based application using HTML, CSS, JavaScript, Python, and SQL. A tool that helps users calculate and manage their weekly spending all in one place.
+
+This project was inspired by my roommate and I's weekly spending review. It was a process that took a lot of our time due to the use of different tools and web pages. As the result, I wanted to build this web-based application to help us and others increase efficiency and reduce the task's time.
+
+<img src="./static/webpage.jpg" alt="homepage" />
+<br />
+
+### FEATURES
+------------
+* Ability to register/log in so that data will not get mixed up with other users
+* Ability to add budget goals to each of the spending category
+* Ability to remove items from Summary Page
+* Ability to input unlimited spending items while seeing the total amount of each category populates
+* A Summary Page that shows a simple yet detailed summary of each spending item that users inputed with a total budget and total amount spent on the top left corner
+
+<br />
+
+### EXECUTING PROGRAM
+------------
+
+#### Running the Program With Flask
+
+* Make sure you're in the 'Final_Project' directory before running the flask command. After running the command, hold 'Ctrl' while clicking on the link to view the website.
+  ```
+  Final_Project/ $ flask run
+  ```
+
+#### Acessing SQL Database
+* In order to access SQL database, you will need to run:
+  ```
+  Final_Project/ $ sqlite3 money_review.db
+  ```
+  * Once you're in sqlite3, run the command '.schema' to see what's inside the database
+
+<br />
+
+### NAVIGATING THE APPLICATION
+------------
+When the application is ran, users will be required to register/log in with username and password.
+
+Once users are in, they could navigate to the Budget tab to start adding their weekly spending budget under each of the five given categories. For the sake of consistency and sorting in the Summary Page, users are only given five common categories such as grocery, eating out, entertainment, pet supply, and other. This will then populate into the Summary Page for users to reference later.
+
+Next, users could go to the Calculate Page to start adding their spending. There is no limit to the number of spending items users get to add. The sum for each category will then populate in a table underneath as users continue to add to the table.
+
+As users are adding items to the Budget Page and the Calculate Page, these data are saved in the SQLite database and are populating in the Summary Page simultaneously. This page is designed to help users look at everything they inputed, as well as removing any items they no longer wanted.
+
+<br />
+
+### UNDERSTANDING THE FILES
+------------
+## **`app.py`**
+<br />
+
+The first thing you will see in this file are a bunch of imports that are helpful in later code. One of these imports is the Flask framework that is used in Python to make writing code simplier.
+
+The next few lines of code show configuring of Flask and ensuring that the templates are auto-reloaded when changes are made to the templates. Then, the file configures `session` to use filesystem instead of Flask's default, signed cookies. This session also has default time limit in which it will expire. In the next line of code, the file configures CS50 Library to use SQLite database called `money_review.db`. The file also ensures users' responses are not cached to prevent different authenticated users from using each other's information. Moving on to the `login_required` function. The purpose of this function is to make sure that users must logged in before they can access certain routes.
+
+The rest of the file contains nine routes that tell Flask when to call each function. Almost all of the routes support GET and POST method, which is a way to transfer data from users to server in HTTP protocol.
+
+<br />
+
+**Login**, **Logout**, **Register**
+
+These three routes that do not have `login_required` function. Let's first take a look at the `login` route. This function unsures users input the correct username and password by using `db.execute` to query into `money_review.db`. This is where usernames and passwords are stored. Also, notice the function `check_password_hash` is used to compare users' hashed passwords. `Session` is used to remember which user has logged in. Next is the `logout` function where it simply uses `session.clear()` to log users out.
+
+Moving on the register route. This route requires users to input a username, password, and confirmation password to sign up. It will render an error page called `error.html` if username/password has already been taken, left blank, or password and confirmation password do not match. In this function, it called the `generate_password_hash()` function to add a layer of security for the users. Once registered via POST, `db.execute` will INSERT the hashed password and username into a database called `users`.
+
+<br />
+
+**Home**, **Index**
+
+The next two routes are solely directing users to the home page. The `home` function is called when users clicked on the home icon which will navigate them to the home page. The `index` function is the default page when users are logged in.
+
+<br />
+
+**Budget**
+
+This first thing in this route is to obtain the user ID. This route requires users to input their weekly budget goals. Once they clicked done, `db.execute` will run and `UPDATE` the budget uniquely to the users into a database called `spendings`.
+
+<br />
+
+**Calculate**
+
+This first thing in this route is to obtain the user ID. This route requires users to fill out the date, category, description, and amount of each spending item. Once they click submit, `db.execute` will run and `INSERT` the these items uniquely to the users into a database called `spendings`.
+
+Each time users submit their spending, `db.execute` will run and `SELECT` the `SUM` of each category and display them in a table on the website.
+
+<br />
+
+**Summary**
+
+This first thing in this route is to obtain the user ID. This route does not require user inputs. `db.execute` will run and `SELECT` information to display on the page.
+
+<br />
+
+**Remove**
+
+This route does not require user inputs. `db.execute` will run and `REMOVE` items from `spendings`.
+
+<br />
+
+## **`static/`**
+<br />
+
+This folder contains CSS files and picture files for HTML templates.
+
+<br />
+
+## **`templates/`**
+<br />
+
+This folder contains eight HTML files.
+
+<br />
+
+**Login, Register, Home**
+
+Both are simply HTML forms with Bootstrap framework for template designs.
+
+<br />
+
+**Error**
+
+Recall register and login routes in `app.py`, two arguments were passed into `render_template` which are `errors.html` and a variable called `message`. In `errors.html`, we were able to call `message` using Jinja.
+
+<br />
+
+**Layout**
+
+This template has the navigation bar that all the other templates use. It uses Jinja's template inheritance to build a base layout that has all the common elements, then inserts blocks that child templates can add to the layout. The navigation bar is also compatible with mobile devices.
+
+<br />
+
+**Budget**
+
+In this template, JavaScript was used to calculate the total budget amount and to create a pop-up alert letting users know that their inputs have been saved.
+
+<br />
+
+**Calculate**
+
+In this template, JavaScript was used to create a pop-up alert letting users know that their inputs have been saved.
+
+<br />
+
+**Summary**
+
+This template has a `remove` button where users can remove any items that they changed their minds on.
+
+<br />
+
+### AUTHOR
+------------
+
+Faith Le <br />
+Email: lefaith12661@gmail.com <br />
+Linkedin: [@Faith Le](https://www.linkedin.com/in/faithle/)
---
+++ Final_Project/app.py
@@ -0,0 +1,226 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+from functools import wraps
+
+
+# __name__ just refers to the name of the current file
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///money_review.db")
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Decorate routes to require login.
+# https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+# log user in
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Forget any id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html", message="must provide username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html", message="must provide password")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("error.html", message="invalid username and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+# log user out
+@app.route("/logout")
+def logout():
+    # Forget any id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/login")
+
+# register user
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # access form data
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # If any field is left blank, return an apology
+        if not username:
+            return render_template("error.html", message="Missing Username")
+        elif not password:
+            return render_template("error.html", message="Missing Password")
+        elif not confirmation:
+            return render_template("error.html", message="Missing Confirmation")
+
+        # If password and confirmation don’t match, return an apology
+        if password != confirmation:
+            return render_template("error.html", message="Passwords Do Not Match")
+
+        # Password hashing adds a layer of security
+        hash = generate_password_hash(password)
+
+        try:
+            # insert data into database
+            new_user = db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
+
+            # go back to homepage
+            return redirect("/")
+        except:
+            # If the username is already taken, return an apology
+            return render_template("error.html", message="Username is already taken")
+
+        # keeps track of which user is logged in
+            session["user_id"] = new_user
+
+    else:
+        return render_template("register.html")
+
+
+# when clicked on home icon, it will navigate to home page
+@app.route("/home")
+@login_required
+def home():
+    return render_template("home.html")
+
+# default page after logged in
+@app.route("/")
+@login_required
+def index():
+    return render_template("home.html")
+
+@app.route("/budget", methods=["GET", "POST"])
+@login_required
+def budget():
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # obtain id
+        id = session["user_id"]
+
+        # access form data
+        wkly_ttl = request.form.get("total_amount")
+
+        # remember item
+        db.execute("UPDATE users SET wkly_ttl = ? WHERE id = ?", wkly_ttl, id)
+
+        # confirmation
+        return redirect("/budget")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("budget.html")
+
+
+@app.route("/calculate", methods=["GET", "POST"])
+@login_required
+def calculate():
+    # obtain id
+    id = session["user_id"]
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # access form data
+        date = request.form.get("date")
+        category = request.form.get("category")
+        description = request.form.get("description")
+        amount = request.form.get("amount")
+
+        # remember item
+        db.execute("INSERT INTO spendings (user_id, date, category, description, amount) VALUES(?, ?, ?, ?, ?)",
+                   id, date, category, description, amount)
+
+        # redirect user to summary
+        return redirect("/calculate")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        # total spending of each item
+        grocery_ttl = db.execute("SELECT ROUND(SUM(amount),2) AS sum_gro FROM spendings WHERE category = 'Grocery' AND user_id = ?", id)
+        eating_out_ttl = db.execute("SELECT ROUND(SUM(amount),2) AS sum_eat FROM spendings WHERE category = 'Eating Out' AND user_id = ?", id)
+        entertainment_ttl = db.execute("SELECT ROUND(SUM(amount),2) AS sum_ent FROM spendings WHERE category = 'Entertainment' AND user_id = ?", id)
+        pet_supply_ttl = db.execute("SELECT ROUND(SUM(amount),2) AS sum_pet FROM spendings WHERE category = 'Pet Supply' AND user_id = ?", id)
+        other_ttl = db.execute("SELECT ROUND(SUM(amount),2) AS sum_oth FROM spendings WHERE category = 'Other' AND user_id = ?", id)
+
+        return render_template("calculate.html", grocery_ttl=grocery_ttl, eating_out_ttl=eating_out_ttl, entertainment_ttl=entertainment_ttl,
+                               pet_supply_ttl=pet_supply_ttl, other_ttl=other_ttl)
+
+# show a summary of the spendings
+@app.route("/summary")
+@login_required
+def summary():
+    # obtain id
+    id = session["user_id"]
+
+    # select information we want to display
+    spendings = db.execute("SELECT id, date, category, description, amount FROM spendings WHERE user_id = ? ORDER BY category", id)
+    users = db.execute("SELECT wkly_ttl FROM users WHERE id = ?", id)
+
+    # sum of all items
+    sum_all = db.execute("SELECT ROUND(SUM(amount),2) as sum_amount FROM spendings WHERE user_id = ?", id)
+
+    return render_template("summary.html", spendings=spendings, users=users, sum_all=sum_all)
+
+# remove item from summary list
+@app.route("/remove", methods=["POST"])
+@login_required
+def remove():
+    # remove spending
+    id = request.form.get("id")
+    if id:
+        db.execute("DELETE FROM spendings WHERE id = ?", id)
+    return redirect("/summary")
+
+
+
+
